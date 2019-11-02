b6d09e4a883014a8e20de393b3f4724360533bf8
agnaev <agnev@mwpost.ru>
2019-11-02 23:41:53 +0300

    almost ready

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,35 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Encode = new System.Windows.Forms.Button();
+            this.decoded = new System.Windows.Forms.TextBox();
+            this.encoded = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // Encode
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(539, 165);
-            this.textBox1.TabIndex = 0;
+            this.Encode.Location = new System.Drawing.Point(270, 167);
+            this.Encode.Name = "Encode";
+            this.Encode.Size = new System.Drawing.Size(138, 23);
+            this.Encode.TabIndex = 0;
+            this.Encode.Text = "Encode /  Decode";
+            this.Encode.UseVisualStyleBackColor = true;
+            this.decoded.TextChanged += new System.EventHandler(this.Encode_TextChanged);
             // 
-            // button1
+            // decoded
             // 
-            this.button1.Location = new System.Drawing.Point(13, 184);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Зашивровать";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.decoded.Location = new System.Drawing.Point(12, 41);
+            this.decoded.Multiline = true;
+            this.decoded.Name = "decoded";
+            this.decoded.Size = new System.Drawing.Size(333, 120);
+            this.decoded.TabIndex = 2;
+            // 
+            // encoded
+            // 
+            this.encoded.Location = new System.Drawing.Point(351, 41);
+            this.encoded.Multiline = true;
+            this.encoded.Name = "encoded";
+            this.encoded.Size = new System.Drawing.Size(342, 120);
+            this.encoded.TabIndex = 3;
+            this.encoded.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(466, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Encoded text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(104, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Source text";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 296);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(705, 296);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encoded);
+            this.Controls.Add(this.decoded);
+            this.Controls.Add(this.Encode);
             this.Name = "Form1";
             this.Text = "Гаммирование";
             this.ResumeLayout(false);
@@ -66,8 +101,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Encode;
+        private System.Windows.Forms.TextBox decoded;
+        private System.Windows.Forms.TextBox encoded;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,87 +13,143 @@ namespace Gamming
 {
     public partial class Form1 : Form
     {
-        private int state;
-        private int A { get; }
-        private int b { get; }
-        private int C { get; }
-        private int M { get; }
-        private int T0 { get; }
-
         public Form1()
         {
             InitializeComponent();
-            state = 1;
-            A = 5;
-            C = 3;
-            b = 8;
-            M = (int)Math.Pow(2, b);
-            T0 = 7;
         }
 
-        private int T(int t)
-        {
-            return (A * t + C) % M;
-        }
+        private readonly char[] characters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+                                                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                                                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                                'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
+                                                'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+                                                'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ',
+                                                'э', 'ю', 'я',' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0' , '?', '.', ',', '!', '@', '#', '$', '%', 
+                                                '^', '&', '*', '(', ')', '+', '=', '<', '>', '`', '!', 
+                                                '\'', '"', '/', '\\', '[', ']', '{', '}', '~', '\r', '\n'};
 
-        private int H(char c)
+        private void Encode_TextChanged(object sender, EventArgs e)
         {
-            int res = 0;
-            for (byte i = 0; i < 32; i++)
+            try
             {
-                if ((1 & (c >> i)) > 0)
+                encoded.Text = DecodeText(decoded.Text, Generate_Pseudorandom_KeyWord(decoded.Text.Length, 10));
+                //decoded.Text = string.Empty;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message + " " + (exc.InnerException?.InnerException?.Message ?? "no inner exc") + (exc.StackTrace ?? " no stack trace"));
+            }
+        }
+
+        private string EncodeText(string input, string keyword)
+        {
+            try
+            {
+                string result = "";
+
+                int keyword_index = 0;
+
+                foreach (char symbol in input)
                 {
-                    res++;
+                    int c = (Array.IndexOf(characters, symbol) +
+                        Array.IndexOf(characters, keyword[keyword_index])) % characters.Length;
+
+                    result += characters[c];
+
+                    keyword_index++;
+
+                    if ((keyword_index + 1) == keyword.Length)
+                        keyword_index = 0;
                 }
+
+                return result;
             }
-
-            return res;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            switch (state)
+            catch(Exception e)
             {
-                case 1://Зашифровать
-                    {
-                        //groupBox1.Text = “Криптограмма“;
-                        button1.Text = "Расшифровать";
-                        SetChars(crypt(GetChars(), true));
-                        state = 2;
-                        break;
-                    }
-                case 2://Расшифровать
-                    {
-                        //groupBox1.Text = “Исходный текст“;
-                        button1.Text = "Зашифровать";
-                        SetChars(crypt(GetChars(), false));
-                        state = 1;
-                        break;
-                    }
+                MessageBox.Show(e.Message + " " + (e.InnerException?.InnerException?.Message ?? "no inner exc") + (e.StackTrace ?? " no stack trace"));
+                return " ";
             }
         }
 
-        private char[] GetChars()
+        private string Generate_Pseudorandom_KeyWord(int length, int startSeed)
         {
-            return textBox1.Text.ToCharArray();
-        }
-
-        private void SetChars(char[] c)
-        {
-            textBox1.Clear();
-            textBox1.Text = new string(c);
-        }
-
-        private char[] crypt(char[] msg, bool f)
-        {
-            int oldh = T0;
-            char[] res = new char[msg.Length];
-            for (int i = 0; i < msg.Length; i++)
+            try
             {
-                res[i] = (char)(T(oldh) ^ msg[i]);
-                oldh = H(f ? msg[i] : res[i]);
+                LCG rand = new LCG(startSeed);
+
+                string result = "";
+
+                for (int i = 0; i < length; i++)
+                {
+                    int position = rand.NextByte(characters.Length).FirstOrDefault() % characters.Length;
+                    result += characters[position];
+                }
+
+                return result;
             }
-            return res;
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message + " " + (e.InnerException?.InnerException?.Message ?? "no inner exc") + (e.StackTrace ?? " no stack trace"));
+                return " ";
+            }
         }
+
+        private string DecodeText(string input, string keyword)
+        {
+            try
+            {
+                string result = "";
+
+                int keyword_index = 0;
+
+                foreach (char symbol in input)
+                {
+                    int posSourceText = GetPosition(characters, symbol);
+                    int posKey = GetPosition(keyword, keyword[keyword_index]);
+                    int p = (posSourceText ^ posKey) % characters.Length;
+
+                    if(posSourceText == -1)
+                    {
+                        throw new Exception($"symbol {symbol} not find in characters array");
+                    }
+                    else if(posKey == -1)
+                    {
+                        throw new Exception($"symbol {keyword[keyword_index]} not find in keyword: {keyword}");
+                    }
+                    //else if(p < 0 || p > characters.Length)
+                    //{
+                    //    p %= characters.Length;
+                    //    //throw new Exception($"xor {posSourceText} ^ {posKey} equls {p}");
+                    //}
+
+                    result += characters[p];
+
+                    keyword_index++;
+
+                    if ((keyword_index + 1) == keyword.Length)
+                        keyword_index = 0;
+                }
+
+                return result;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message + " " + (e.InnerException?.InnerException?.Message ?? "no inner exc") + (e.StackTrace ?? " no stack trace"));
+                return "";
+            }
+        }
+
+        public int GetPosition<T>(T[] arr, T elem) => Array.IndexOf(arr, elem);
+        public int GetPosition(string arr, char elem)
+        {
+            char[] chararray = arr.ToArray();
+            var result = GetPosition<char>(chararray, elem);
+            return result;
+         }
     }
 }
